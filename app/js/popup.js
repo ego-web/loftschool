3080e4d4c1e04c9791b3e0fe1b9085ba0f471ae9
ego_web <ego_web@mail.ru>
2016-02-05 19:19:33 +0300

    change popup

---
+++ app/js/popup.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ego on 03.02.2016.
+ */
+    $(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы
+        PopUpHide();
+    });
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+}
