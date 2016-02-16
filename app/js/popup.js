e39ae43d5088051ab5b253a8388dbcb81f4dfdad
ego_web <ego_web@mail.ru>
2016-02-16 14:55:52 +0300

    add validation

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,15 +1,58 @@
-/**
- * Created by ego on 03.02.2016.
- */
     $(document).ready(function(){
-        //Скрыть PopUp при загрузке страницы
-        PopUpHide();
+        var validation = {
+            'fullName': function () {
+                $('body').append('<div id="nameInfo" class="error"></div>');
+                var nameInfo = $('#nameInfo');
+                var elem = $('#name');
+                var pos = elem.offset();
+                //var cs = window.getComputedStyle(document.getElementById('name'), "");
+                //alert(cs.getPropertyValue("title"));
+                if (this.getAttribute('title') != 'left') {
+                    nameInfo.css({
+                        top: pos.top,
+                        left: pos.left - elem.width()
+                    });
+                } else {
+                    nameInfo.css({
+                        top: pos.top,
+                        left: pos.left + elem.width()
+                    });
+                }
+                if (elem.val().trim() == "") {
+                    validation.errors = true;
+                    nameInfo.removeClass('correct').addClass('error').html('← at least 6 characters').show();
+                    elem.removeClass('normal').addClass('wrong');
+                } else {
+                    nameInfo.removeClass('error').addClass('correct').html('√').show();
+                    elem.removeClass('wrong').addClass('normal');
+                }
+            },
+
+/*            'sendIt': function () {
+                if (!validation.errors) {
+                    $('#jform').submit();
+                }
+            }*/
+        };
+            $('#sendform').click(function (e){
+                e.preventDefault();
+alert( validation.fullName)
+
+/*                var obj = $.browser ? $('body') : $('html');
+            obj.animate({ scrollTop: $('#jform').offset().top }, 750, function (){
+                validation.errors = false;
+                validation.fullName();
+                validation.sendIt();
+            });
+            return false;*/
+        });
+        $('#name').change(validation.fullName);
     });
-//Функция отображения PopUp
-function PopUpEnterShow(){
-    $("#popup1").show();
+
+function PopUpEnterShow() {
+    $("#popup1").fadeIn(600);
 }
 //Функция скрытия PopUp
-function PopUpHide(){
-    $("#popup1").hide();
+function PopUpHide() {
+    $("#popup1").fadeOut(200);
 }
---
+++ app/js/validation.js
@@ -0,0 +1,19 @@
+var validation = (function () {
+
+    //initializing modul
+    var init = function () {
+        _setUpListeners();
+    };
+
+    //listen events
+    var _setUpListeners = function () {
+
+    };
+    //return object (public method)
+    return {
+        init: init
+    };
+
+})();
+
+validation.init();
