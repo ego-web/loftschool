526aecb1494bded6b9fa7505a58b59e61dcb1add
ego_web <ego_web@mail.ru>
2016-02-20 18:43:55 +0300

    upload image form

--- gulpfile.js
+++ gulpfile.js
@@ -13,26 +13,9 @@ gulp.task('server', function () {
 gulp.task('watch', function () {
 	gulp.watch([
 		'app/*.html',
-		'app/js/!**!/!*.js',
-		'app/css/!**/!*.css'
+		'app/js/**/*.js',
+		'app/css/**/*.css'
 	]).on('change', browserSync.reload);
 });
 
 	gulp.task('default', ['server', 'watch']);
-
-/*
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    livereload = require('gulp-livereload');
-
-gulp.task('less', function() {
-	gulp.src('less/!*.less')
-		.pipe(less())
-		.pipe(gulp.dest('css'))
-		.pipe(livereload());
-});
-
-gulp.task('watch', function() {
-	livereload.listen();
-	gulp.watch('less/!*.less', ['less']);
-});*/
